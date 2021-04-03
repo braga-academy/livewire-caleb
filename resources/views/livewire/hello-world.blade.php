c4b50e964240fdb76b947236dcdfc2b2c614c2de
lucianobragaweb <lucianobragaweb@gmail.com>
2021-04-03 13:42:26 -0300

    data binding

--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -1,3 +1,13 @@
 <div>
-    Hello {{ $name }}!
+    <div>
+        <input wire:model="name" type="text">
+        <input type="checkbox" wire:model="loud">
+
+        <select wire:model="greeting" multiple>
+            <option>Hello</option>
+            <option>Goodbye</option>
+            <option>Adios</option>
+        </select>
+    </div>
+    {{ implode(', ', $greeting) }} {{ strtoupper($name) }} @if($loud)! @endif
 </div>
