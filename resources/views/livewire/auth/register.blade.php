0a450bd3ca4b955ddbd496b36119c62a42217ea9
lucianobragaweb <lucianobragaweb@gmail.com>
2021-04-03 16:29:39 -0300

    setting up the form component

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,3 +1,21 @@
-<div>
-    Hello
-</div>
+<form wire:submit.prevent="register">
+    <div>
+        <label for="name">name</label>
+        <input wire:model="name" type="text" id="name" name="name">
+    </div>
+    <div>
+        <label for="email">email</label>
+        <input wire:model="email" type="text" id="email" name="email">
+    </div>
+    <div>
+        <label for="password">password</label>
+        <input wire:model="password" type="password" id="password" name="password">
+    </div>
+    <div>
+        <label for="passwordConfirmation">passwordConfirmation</label>
+        <input wire:model="passwordConfirmation" type="password" id="passwordConfirmation" name="passwordConfirmation">
+    </div>
+    <div>
+        <input type="submit" value="Register">
+    </div>
+</form>
