07ed4fcc00a1db2e55c95da21cd0d6cd9e7b6713
lucianobragaweb <lucianobragaweb@gmail.com>
2021-04-03 16:47:50 -0300

    adding validation

--- app/Http/Livewire/Auth/Register.php
+++ app/Http/Livewire/Auth/Register.php
@@ -13,13 +13,23 @@ class Register extends Component
     public $password = '';
     public $passwordConfirmation = '';
 
+    protected $rules = [
+        'name' => 'required',
+        'email' => 'required|email|unique:users',
+        'password' => 'required|min:6|same:passwordConfirmation',
+    ];
+
     public function register()
     {
+        $this->validate();
+
         User::create([
             'name' => $this->name,
             'email' => $this->email,
             'password' => Hash::make($this->password)
         ]);
+
+        return redirect('/');
     }
 
     public function render()
