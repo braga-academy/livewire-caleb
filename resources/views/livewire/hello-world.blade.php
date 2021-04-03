f8631c54ee8e01670eb7c29ac838d4eb23264bd9
lucianobragaweb <lucianobragaweb@gmail.com>
2021-04-03 13:58:25 -0300

    livewire actions

--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -9,5 +9,17 @@
             <option>Adios</option>
         </select>
     </div>
-    {{ implode(', ', $greeting) }} {{ strtoupper($name) }} @if($loud)! @endif
+
+    <div>
+        {{ implode(', ', $greeting) }} {{ strtoupper($name) }} @if($loud)! @endif
+    </div>
+
+    <div>
+        <form action="#" wire:submit.prevent="$set('name', 'Bingo')">
+            <button type="submit">Reset Name</button>
+        </form>
+        <form action="#" wire:submit.prevent="resetName('Mario')">
+            <button type="submit">Reset Name</button>
+        </form>
+    </div>
 </div>
